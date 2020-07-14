7d62fce4b13a89e7d584ebe46cdbf9953dcf96f6
Ahmed Hadjou <thehidjou@yahoo.com>
2020-07-14 19:44:33 +0100

    Storing user id with message, installed twilio sdk

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Twilio\Rest\Client as TwilioClient;
 
 use App\Entity\Message;
 use App\Form\MessageType;
@@ -22,19 +24,16 @@ class PageController extends AbstractController
     public function index(Request $request): Response
     {
         $message = new Message();
-        
-        // // Create a message and persist it (testing)
-        // $message->setText('Hi Ahmed!');
-        // $message->setUserId(1);
-        // $message->setRecipient('00447459188428');
-        // $em = $this->getDoctrine()->getManager();
-        // $em->persist($message);
-        // $em->flush();
+
+        // Get current user
+        $user = $this->getUser();
+            
+        // Attach user id to message
+        $message->setUserId($user->getId());
 
         // TODO: Validate data (text not empty, recipient not empty and numerical)
 
-        // TODO: Trim number, remove white spaces, replace '+' with 00
-
+        // TODO: Trim number, remove white spaces, ?? replace '+' with 00 ??
         
 
         $form = $this->createForm(MessageType::class, $message);
@@ -43,9 +42,25 @@ class PageController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             // $formData = $form->getData();
 
+            // Accessing .env vars with '$_ENV()' as I couldnt find the equivalent in Symfony quickly haha
+            $twilio = new TwilioClient($_ENV['TWILIO_SID'], $_ENV['TWILIO_AUTH_TOKEN']);
+
             $em = $this->getDoctrine()->getManager();
             $em->persist($message);
             $em->flush();
+
+            // $message = $twilio->messages->create(
+            //     // the number you'd like to send the message to
+            //     // +447591339388
+            //     $message->getRecipient(),
+            //     [
+            //         // A Twilio phone number you purchased at twilio.com/console
+            //         // TODO: Get verified numbers from DB
+            //         "from" => "+17205839384",
+            //         // the body of the text message you"d like to send
+            //         "body" => $message->getText()
+            //     ]
+            // );
 
             return $this->redirectToRoute('page.success');
         }
@@ -62,5 +77,33 @@ class PageController extends AbstractController
     public function success(): Response
     {
         return $this->render('page/success.html.twig');
+    }
+
+    /**
+     * TODO: Remove this
+     * Test route
+     * @Route("/test", name="test")
+     */
+    public function test(): Response
+    {
+        // Accessing .env vars with '$_ENV()' as I couldnt find the equivalent in Symfony quickly haha
+        $twilio = new TwilioClient($_ENV['TWILIO_SID'], $_ENV['TWILIO_AUTH_TOKEN']);
+
+        $message = $twilio->messages->create(
+            // the number you'd like to send the message to
+            // +447591339388
+            "+447459188428",
+            [
+                // A Twilio phone number you purchased at twilio.com/console
+                "from" => "+17205839384",
+                // the body of the text message you"d like to send
+                "body" => "Hi Ahmed!"
+            ]
+        );
+
+        dump($message);
+        die;
+
+        return new Response("<h1>" . $_ENV['APP_NAME'] . "</h1>");
     }
 }
